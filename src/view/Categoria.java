a287e2c44f1df597e6c081ca8c7cb132478270af
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-10-22 11:12:28 -0300

    ATP29 - Criando classe categoria.

---
+++ src/view/Categoria.java
@@ -0,0 +1,31 @@
+package view;
+
+public class Categoria extends Base {
+    public String nome;
+    public String descricao;
+
+    /**
+     * Cria id
+     */
+    public Categoria() {
+        //seta id
+        super();
+    }
+
+    /**
+     * Inicializa os atributos e cria o id
+     * @param nome
+     * @param descricao
+     */
+    public Categoria(String nome, String descricao) {
+        super();
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+    
+    @Override
+    public String toString() {
+        String texto = "Categoria: " + this.nome + " - " + this.descricao;
+        return texto;
+    }
+}
