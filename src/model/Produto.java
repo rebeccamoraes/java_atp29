e441b8747f3548d2fd377e1843504af2f4478f81
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-22 11:21:40 -0300

    ATP29 - Criação da classe Produto

---
+++ src/model/Produto.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Produto extends Base {
+    public String nome;
+    public Categoria categoria;
+    public double preco;
+    public int quantidade;
+
+    /** Construtor genérico para inicializar id */
+    public Produto() {
+        super();
+    }
+
+    /**
+     * Construtor para inicializar os atributos e id
+     * 
+     * @param nome       nome do produto
+     * @param categoria  categoria do produto
+     * @param preco      preço do produto
+     * @param quantidade quantidade do produto
+     */
+    public Produto(String nome, Categoria categoria, double preco, int quantidade) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNome: " + this.nome + this.categoria + String.format("\nPreço: R$%.2f", this.preco) + "\nQuantidade: "
+                + this.quantidade;
+    }
+}
