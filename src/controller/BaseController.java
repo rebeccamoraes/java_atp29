785ffadb54df1fec3dc41d99cbb943ef04422511
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-22 11:37:07 -0300

    ATP29 - Crud update e delete

--- src/controller/BaseController.java
+++ src/controller/BaseController.java
@@ -10,7 +10,8 @@ public class BaseController<T> {
     }
 
     /**
-     * Adiciona um intem à lista
+     * Adiciona um item à lista
+     * 
      * @param item objeto a ser adicionado
      */
     public void create(T item) {
@@ -19,13 +20,32 @@ public class BaseController<T> {
 
     /**
      * Retorna a lista
+     * 
      * @return itens do ArrayList
      */
     public ArrayList<T> read() {
         return this.lista;
     }
 
-    //update
+    /**
+     * Verifica se o ArrayList contém um item com o mesmo id do item recebido. Se
+     * sim, o item presente no ArrayList é excluído e o item recebido é adicionado.
+     * 
+     * @param item objeto a ser verificado e adicionado
+     */
+    public void update(T item) {
+        if (this.lista.contains(item)) {
+            this.delete(item);
+            this.create(item);
+        }
+    }
 
-    //delete
+    /**
+     * Remove o objeto recebido do ArrayList
+     * 
+     * @param item objeto a ser removido
+     */
+    public void delete(T item) {
+        this.lista.remove(item);
+    }
 }
